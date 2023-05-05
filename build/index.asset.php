61da7a82cf2f6d8108d0ad04a167846ada60fa03
Corentin Gautier <corentin.gautier@consertotech.pro>
2023-05-05 09:50:24 +0200

    fix: admin button display

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => 'f615f82c3f0a6569d980');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => '392c3e9efde70196c785');
