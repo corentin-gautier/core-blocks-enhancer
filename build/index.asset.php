739c2fe3edbbfb29792a43fc2c2bd496f3bf3016
Corentin Gautier <corentin.gautier@consertotech.pro>
2024-02-19 13:37:58 +0100

    fix: forgot to generate assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => 'b0920bbed320a6c0cda7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => '9e09abf2d6b3db53c181');
