55942da9ebb1f8fc80db631686598b41f0c8f4ab
Corentin Gautier <corentin.gautier@consertotech.pro>
2024-02-19 10:35:08 +0100

    fix: backoffice buttons icon alignement

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => 'bf73bf9b1da1d59e07e8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => 'b0920bbed320a6c0cda7');
