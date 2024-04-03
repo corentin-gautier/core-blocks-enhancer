52b574a6ad7effd4a308f612e305eff26e9d12c0
Corentin Gautier <corentin.gautier@consertotech.pro>
2024-04-03 14:28:48 +0200

    fix: buttons admin alignment

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => '9e09abf2d6b3db53c181');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => '7f215dc07bce1435ff3f');
